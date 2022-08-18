1224b3720d56ce69545fa6b0bb34fb813c8a9ef4
ptamayova <ptamayocode@gmail.com>
2022-08-18 11:13:04 +0200

    Reinstalls Doctrine Maker Bundle and adds new Answer entity

--- config/bundles.php
+++ config/bundles.php
@@ -11,6 +11,7 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
