b1a76fa434fb20377a689da737b7a6fa7eb2bf33
ptamayova <ptamayocode@gmail.com>
2022-08-18 09:25:58 +0200

    Adds QuestionFactory logic and installs StofDoctrineExtensionsBundle

---
+++ src/Factory/QuestionFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Factory;
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
+use DateTime;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+/**
+ * @method static Question|Proxy findOrCreate(array $attributes)
+ * @method static Question|Proxy random()
+ * @method static Question[]|Proxy[] randomSet(int $number)
+ * @method static Question[]|Proxy[] randomRange(int $min, int $max)
+ * @method static QuestionRepository|RepositoryProxy repository()
+ * @method Question|Proxy create($attributes = [])
+ * @method Question[]|Proxy[] createMany(int $number, $attributes = [])
+ */
+final class QuestionFactory extends ModelFactory
+{
+    protected function getDefaults(): array
+    {
+        return [
+        'name' => self::faker()->realText(50),
+        'question' => self::faker()->paragraphs(
+            self::faker()->numberBetween(1, 4),
+            true
+        ),
+        'askedAt' => self::faker()->dateTimeBetween('-100 days', '-1 minute'),
+        'votes' => rand(-20, 50)
+    ];
+    }
+    protected function initialize(): self
+    {
+        return $this
+             ->afterInstantiate(function(Question $question) {
+                 if (!$question->getSlug()) {
+                     $slugger = new AsciiSlugger();
+                     $slugFromQuestionName = $slugger->slug($question->getName());
+                     $question->setSlug(strtolower($slugFromQuestionName));
+                 }
+            });
+    }
+
+    public function unpublished(): self
+    {
+        return $this->addState(['askedAt' => null]);
+    }
+
+    protected static function getClass(): string
+    {
+        return Question::class;
+    }
+}
